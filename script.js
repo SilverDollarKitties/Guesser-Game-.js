c926d2299a86eca189ef91c104b6349b61487d30
SilverDollarKitties <105564724+SilverDollarKitties@users.noreply.github.com>
2023-03-13 14:52:15 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,26 +2,28 @@ let humanScore = 0;
 let computerScore = 0;
 let currentRoundNumber = 1;
 
-const generateTarger = () => {
-  Math.floor(Math.random() * 10);
+// Write your code below:
+const generateTarget = () => {
+    return Math.floor(Math.random() * 10);
 }
 
-const compareGuesses = (humanGuess, computerGuess, targetGuess) => {
-const humandiff = Math.abs(targetGuess - humanGuess);
-const computerdiff = Math.abs(targetGuess - computerGuess);
+const getAbsoluteDistance = (target, number) => Math.abs(target - number);
 
-return humandiff <= computerdiff;
-};
+const compareGuesses = (userGuess, computerGuess, targetNumber) => {
+    const userAbsDistance = getAbsoluteDistance(targetNumber, userGuess);
+    const computerAbsDistance = getAbsoluteDistance(targetNumber, computerGuess);
+
+    return userAbsDistance <= computerAbsDistance;
+}
 
 const updateScore = winner => {
-  if(winner === 'human'){
-    humanScore++;
-  } else if(winner === 'computer'){
-    computerScore++;
-  }
-};
+    if (winner === 'human')
+        humanScore++;
+    else if (winner === 'computer')
+        computerScore++
+}
 
 const advanceRound = () => {
-  currentRoundNumber++;
+    currentRoundNumber++;
 }
 
